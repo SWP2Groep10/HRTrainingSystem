a9b3230237034d3a7d5ae3caf78cdc14f5cab83e
TomVanlaerEHB <tom.vanlaer@student.ehb.be>
2017-10-25 13:14:08 +0200

    Revert "Revert "Made few changes based on feedback and created the remaining classes which are not finished yet""
    This reverts commit abeba5933705f80f06f2c5ceb30e9e032a137253.

--- src/logic/Location.java
+++ src/logic/Location.java
@@ -4,7 +4,6 @@ public class Location {
 
 	private Address address;
 	private String locationName;
-	private int latitude, longitude;
 	
 	public Address getAddress() {
 		return address;
@@ -18,29 +17,14 @@ public class Location {
 	public void setLocationName(String locationName) {
 		this.locationName = locationName;
 	}
-	public int getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(int latitude) {
-		this.latitude = latitude;
-	}
-	public int getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(int longitude) {
-		this.longitude = longitude;
-	}
 	
 	@Override
 	public String toString() {
-		return "Location [address=" + address + ", locationName=" + locationName + ", latitude=" + latitude
-				+ ", longitude=" + longitude + "]";
+		return "Location [address=" + address + ", locationName=" + locationName + "]";
 	}
-	public Location(Address address, String locationName, int latitude, int longitude) {
+	public Location(Address address, String locationName) {
 		super();
 		this.address = address;
 		this.locationName = locationName;
-		this.latitude = latitude;
-		this.longitude = longitude;
 	}
 }
